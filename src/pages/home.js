9b6102a04890b1f08ff6c9040f1840cf05bf77ed
Nikita <khakimov.nikita.vch@gmail.com>
2023-02-14 22:33:18 +0200

    paginationGraphql

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import NoteFeed from '../components/NoteFeed';
+import Button from '../components/Button';
 
 const GET_NOTES = gql`
   query NoteFeed($cursor: String) {
@@ -23,12 +24,40 @@ const GET_NOTES = gql`
 `;
 
 const Home = () => {
+  const loadingNotes = () => {
+    fetchMore({
+      variables: {
+        cursor: data.noteFeed.cursor,
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        return {
+          noteFeed: {
+            cursor: fetchMoreResult.noteFeed.cursor,
+            hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+            notes: [
+              ...previousResult.noteFeed.notes,
+              ...fetchMoreResult.noteFeed.notes,
+            ],
+            __typename: 'noteFeed',
+          },
+        };
+      },
+    });
+  };
+
   const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
 
-  return <NoteFeed notes={data.noteFeed.notes} />;
+  return (
+    <>
+      <NoteFeed notes={data.noteFeed.notes} />
+      {data.noteFeed.hasNextPage && (
+        <Button onClick={loadingNotes}>Load more</Button>
+      )}
+    </>
+  );
 };
 
 export default Home;
